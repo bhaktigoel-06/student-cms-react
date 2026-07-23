4e116c3da1014be5f5765f11d09db3d86ec74bda
bhakti <bhaktigoel01@gmail.com>
2026-07-23 19:10:15 +0530

    CourseFilter renders

---
+++ student-cms-react/src/component/CourseFilter.jsx
@@ -0,0 +1,10 @@
+export default function CourseFilter() {
+  return (
+    <select>
+      <option>All</option>
+      <option>BCA</option>
+      <option>BTech</option>
+      <option>MCA</option>
+    </select>
+  );
+}
